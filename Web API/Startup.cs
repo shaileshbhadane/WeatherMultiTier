058898accf213aa6b2f6b1ad7217a067ff5fafe6
shaileshbhadane <shailesh_patil@rams.com.au>
2022-03-16 20:30:37 +0530

    test build trigger

--- Web API/Startup.cs
+++ Web API/Startup.cs
@@ -27,7 +27,7 @@ namespace weather_retrieval
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services. //Test
             services.AddMvc();
         }
 
