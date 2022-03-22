1f601b36de28fc86a141a8499e4d3c5d1b1e0ac8
shaileshbhadane <shailesh_patil@rams.com.au>
2022-03-22 14:42:23 +0530

    t2

--- Web API/Startup.cs
+++ Web API/Startup.cs
@@ -32,8 +32,6 @@ namespace weather_retrieval
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // demo test
-        //Faisal request testf
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
